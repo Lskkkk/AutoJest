d904d664ac7f8bad1f22f87770cf09f6e6ca3bd8
skliu <skliu@ctrip.com>
2018-10-20 15:37:14 +0800

    fix: 修复配置

--- Common/index.js
+++ Common/index.js
@@ -2,9 +2,7 @@ function getFuncParamList(func) {
     const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
     const ARGUMENT_NAMES = /([^\s,]+)/g;
     const fnStr = func.toString().replace(STRIP_COMMENTS, '');
-    const result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-    if(result === null)
-       result = [];
+    const result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES) || [];
     return result;
 }
 
